1a89352f9e131838c6c9ee1dce35b58971a1ca08
enviy <8320367+Enviy@users.noreply.github.com>
2022-10-09 15:33:42 -0700

    add credential parameters to config yaml

--- config/config.go
+++ config/config.go
@@ -12,22 +12,39 @@ import (
 //go:embed config.yaml
 var c []byte
 
+// New manages loading configs.
 func New() (Provider, error) {
-	var provider Provider
-	err := yaml.Unmarshal(c, &provider)
+	var prov Provider
+	err := yaml.Unmarshal(c, &prov)
 	if err != nil {
-		return provider, err
+		return prov, err
 	}
 	// os is used intead of commiting credentials to config file.
-	cfg := oauth1.NewConfig(os.Getenv("TWT_CONSUMER_KEY"), os.Getenv("TWT_CONSUMER_SECRET"))
-	token := oauth1.NewToken(os.Getenv("TWT_ACCESS_TOKEN"), os.Getenv("TWT_ACCESS_TOKEN_SECRET"))
+	cfg := oauth1.NewConfig(os.Getenv(prov.Twitter.Key), os.Getenv(prov.Twitter.KeySecret))
+	token := oauth1.NewToken(os.Getenv(prov.Twitter.Token), os.Getenv(prov.Twitter.TokenSecret))
 	// http.Client should auto authorize reqs
 	httpClient := cfg.Client(oauth1.NoContext, token)
-	provider.Client = httpClient
-	return provider, nil
+	prov.Client = httpClient
+	return prov, nil
 }
 
+// Provider defines the aviary config.
 type Provider struct {
-	Client *http.Client
-	Users  []string `yaml:"users"`
+	Client  *http.Client
+	Users   []string `yaml:"users"`
+	Twitter Twitter  `yaml:"twitter"`
+	Azure   Azure    `yaml:"azure"`
+}
+
+// Twitter defines required twitter credentials.
+type Twitter struct {
+	Key         string `yaml:"key"`          // consumer key
+	KeySecret   string `yaml:"key_secret"`   // consumer key secret
+	Token       string `yaml:"token"`        // access token
+	TokenSecret string `yaml:"token_secret"` // access token secret
+}
+
+// Azure defines required azure credentials.
+type Azure struct {
+	InsightsKey string `yaml:"insights_key"`
 }
